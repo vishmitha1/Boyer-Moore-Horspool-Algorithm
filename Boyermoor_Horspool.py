d0e372aab588156b21cb2a94160bcc0d5fe875d5
visal <visaluni2@gmail.com>
2023-07-30 02:42:05 +0530

    Complete boyermoore Algorithm

--- Boyermoor_Horspool.py
+++ Boyermoor_Horspool.py
@@ -1,19 +1,18 @@
-p="pattern1.txt"
+p="pattern1.txt"                                    #read patern file
 with open(p,'r') as file:
     pattern1=file.read()
 t="text1.txt"
-with open(t,'r') as file:
+with open(t,'r') as file:                           #read text file
     text=file.read()
 
-file=open('output.txt','w')
+file=open('output.txt','w')                         #open outputfile
 table={}
 
-def rightmost(pattern, character):
+def rightmost(pattern, character):                  #this finction helps to find rightmost characters indx
     return pattern.rfind(character)
         
 
-
-def HpBc(pattern):
+def HpBc(pattern):                                  #This function helps to Create HpBc Table
     temp=len(pattern)-1
     for x in pattern:
         if(x!= pattern[temp]):
@@ -22,25 +21,22 @@ def HpBc(pattern):
             else:
                 table[x]=0   
 
-def CheckPaternMatch(possition,text,pattern):
+def CheckPaternMatch(possition,text,pattern):        #this Function use for to checking pattern matching
     substring = text[possition:possition+len(pattern)]
     if(substring==pattern):
         return True
     else:
         return False
         
-
-#HpBc(pattern1)    
-#print(table)
 # Next shift possition = position + table[text[possition+length-1]]
 
-def Boyermoore(text,pattern):
+def Boyermoore(text,pattern):                       # in this function calling all other sub functions                      
     possition=0
-    matchings=True
+    matchings=0
     HpBc(pattern)
     while(possition<len(text)-len(pattern)):
         if (CheckPaternMatch(possition,text,pattern)==True):
-            #print("Pattern found at index :" , possition)
+            print("Pattern found at index :" , possition)
             file.write("Pattern found at index  : %d \n" % possition)
             matchings +1
         if text[possition+len(pattern)-1] in table:
@@ -51,6 +47,7 @@ def Boyermoore(text,pattern):
         
         if(matchings==0):
             print("Pattern not found in given text ")
+            file.write("Pattern not found in given text ")
 
 
 Boyermoore(text,pattern1)
